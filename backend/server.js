c0cfd56696c113d36a07c21203f94cbd1c27deaa
Nishant Singh <nishantprince931@gmail.com>
2026-09-15 19:48:18 +0530

    Increase JSON body limit for long typing tests

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ const customPracticeRoutes = require("./routes/customPractice");
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "5mb" }));
 
 app.locals.pool = pool;
 app.use("/api/auth", authRoutes);
